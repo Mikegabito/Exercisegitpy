1b0756e21f36c746eb1c286d0c18362e4ddffa63
Mikegabito <nmaida@gmail.com>
2025-11-25 19:55:40 +0100

    Add Exercisegitpy

---
+++ exercise.py
@@ -0,0 +1,106 @@
+## TASK 1 – VARIABLE 1
+name = "Nelson"
+age = 48
+is_student = True
+
+print(age)
+print(age)
+print(is_student)
+
+## TASK 2 – VARIABLE 2
+city = "Brescia"
+temperature = 3
+is_sunny = False
+
+print(f"Today in {city}, the temperature is {temperature}°C and sunny: {is_sunny}")
+
+## TASK 3 – VARIABLE 3
+num1 = 12
+num2 = 4
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Division:", num1 / num2)
+
+## TASK 4 – VARIABLE 4
+first_name = "Nelson"
+last_name = "Campos"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+## TASK 5 – VARIABLE 5
+price = 19.99
+quantity = 3
+
+total_cost = price * quantity
+print("Total cost:", total_cost)
+
+
+## TASK 6 – VARIABLE 6
+length = 5
+width = 3
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area:", area)
+print("Perimeter:", perimeter)
+
+## TASK 7 – VARIABLE 7
+year_of_birth = 1977
+current_year = 2025
+
+age = current_year - year_of_birth
+print("Your age is:", age)
+
+
+##TASK 8 – VARIABLE 8
+product_name = "Laptop"
+product_price = 899.99
+in_stock = True
+
+print(f"Product: {product_name} — Price: ${product_price} — In Stock: {in_stock}")
+
+
+## TASK 9 – VARIABLE 9
+kilometers = 10
+miles = kilometers * 0.62137
+
+print(f"{kilometers} km is equal to {miles} miles")
+
+
+## TASK 10 – VARIABLE 10
+hours_worked = 40
+hourly_rate = 20
+
+weekly_salary = hours_worked * hourly_rate
+print("Weekly salary:", weekly_salary)
+
+
+## Bonus
+a = "Hello"
+b = 23
+c = 4.5
+d = True
+e = ["Python", "AI", "Data"]
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+## Mini bio
+name = "Nelson"
+hobby = "traveling"
+favorite_food = "pasta"
+city = "Brescia"
+
+print(f"My name is {name}, I live in {city}, I love {hobby}, and my favorite food is {favorite_food}.")
+
+##Hardcode 3 foods
+food1 = "pupusas"
+food2 = "pizza"
+food3 = "tacos"
